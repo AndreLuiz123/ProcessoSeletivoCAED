8c0d25b5612c26cb3bdb96722594483edbc377f1
AndreLuiz123 <luizandr30@yahoo.com>
2020-08-21 20:08:07 -0300

    Cria classe Palavra, verifica canonicidade

--- index.php
+++ index.php
@@ -1,5 +1,94 @@
 <?php
-    include('conexao.php');
+    include("conexao.php");
+
+    function preparaPalavra($string){
+        $vowels = array("<b>","</b>","<u>","</u>");
+        $palavra = str_replace($vowels, "", $string);
+        echo $palavra;
+    }
+
+    function verificaCanonicidade($string){
+        $palavraAnalisada = $string.".";
+        while(strlen($palavraAnalisada)>0)
+        {
+            echo $palavraAnalisada."<br>";
+            $silaba = strstr($palavraAnalisada, '.',true); 
+            if(!$silaba)
+            $silaba = strstr($palavraAnalisada, '',true);
+            echo $silaba."<br>";
+            if(strlen($silaba)>2)
+            {
+                echo "Não é canônica pois tem mais de duas letras na sílaba";
+                return false;
+            }else if(!(!verificaVogal($silaba[0]) && verificaVogal($silaba[1])))
+            {
+                echo "Não é canônica pois não é consoante seguida de vogal";
+                return false;
+            }
+            echo "prox passo <br><br>";
+            $palavraAnalisada = strstr($palavraAnalisada,'.');
+            $palavraAnalisada = substr($palavraAnalisada, 1);
+        }
+        echo "É canônica";
+        return true;
+    }
+
+    function verificaTipo($string){
+        
+
+    }
+
+    function verificaVogal($caractere){
+
+        echo "<br>".$caractere."<br>";
+        switch ($caractere){
+            case 'a':
+                echo "vogal <br>";
+                return true;
+            break;
+            case 'e':
+                echo "vogal <br>";
+                return true;
+            break;
+            case 'i':
+                echo "vogal <br>";
+                return true;
+            break;
+            case 'o':
+                echo "vogal <br>";
+                return true;
+            break;
+            case 'u':
+                echo "vogal <br>";
+                return true;
+            break;
+            case 'A':
+                echo "vogal <br>";
+                return true;
+            break;
+            case 'E':
+                echo "vogal <br>";
+                return true;
+            break;
+            case 'I':
+                echo "vogal <br>";
+                return true;
+            break;
+            case 'O':
+                echo "vogal <br>";
+                return true;
+            break;
+            case 'U':
+                echo "vogal <br>";
+                return true;
+            break;
+            default:
+                echo "consoante <br>";
+                return false;
+            break;
+        }
+    }
+
     $filename = "dicionario.txt";
     $content = file_get_contents($filename);
 
@@ -12,11 +101,66 @@
     $resultado = preg_match_all($pattern, $content, $match);
 
 
-    //$size = count($match[1]);
+    $size = count($match[1]);
+    $novaPalavra = $match[1][20];
 
+
+    //$sql = "INSERT INTO palavra (caracteres, canonicidade, tipo) VALUES ('$novaPalavra', 'false',2)";
+    //$conexao->query($sql);
+
+    
     //for($i = 0; $i<$size; $i++){
-    //    echo ($i+1)." - ".$match[1][$i] ;
-    //    echo "<br>";
-    //}
+        //echo ($i+1)." - ".$match[1][20]."<br>";
+        //preparaPalavra($match[1][20]);
+        verificaCanonicidade("ba.bar");
+        //echo verificaVogal('p');
+        //echo "<br>";
 
+    //}
+?>
+
+<?php
+    class Palavra
+    {
+        public $codigo;
+        public $caracteres;
+        public $canonicidade;
+        public $tipo;
+
+        function Palavra($string){
+
+        }
+        
+        function preparaPalavra($string){
+            $vowels = array("<b>","</b>","<u>","</u>");
+            $palavra = str_replace($vowels, "", $string);
+            return $palavra;
+        }
+
+        function verificaCanonicidade($string){
+
+            $palavraAnalisada = $string.".";
+            while(strlen($palavraAnalisada)>0)
+            {
+                $silaba = strstr($palavraAnalisada, '.',true); 
+                if(!$silaba)
+                $silaba = strstr($palavraAnalisada, '',true);
+                if(strlen($silaba)>2)
+                {
+                    return false;
+                }else if(!(!verificaVogal($silaba[0]) && verificaVogal($silaba[1])))
+                {
+                    return false;
+                }
+                $palavraAnalisada = strstr($palavraAnalisada,'.');
+                $palavraAnalisada = substr($palavraAnalisada, 1);
+            }
+            return true;
+        }
+
+        function verificaTipo($string){
+
+        }
+
+    }
 ?>
